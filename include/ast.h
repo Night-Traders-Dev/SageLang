c8356406b44ee742cf82e68e5886fc86988b81fe
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-11-27 16:41:35 -0500

    Add AST nodes for dict, tuple, slice, break, continue (Phase 5)

--- include/ast.h
+++ include/ast.h
@@ -44,6 +44,25 @@ typedef struct {
     Expr* index;
 } IndexExpr;
 
+// Dictionary literal: {"key1": val1, "key2": val2}
+typedef struct {
+    char** keys;     // Array of string keys
+    Expr** values;   // Array of value expressions
+    int count;
+} DictExpr;
+
+// Tuple literal: (val1, val2, val3)
+typedef struct {
+    Expr** elements;
+    int count;
+} TupleExpr;
+
+// Slice expression: arr[start:end]
+typedef struct {
+    Expr* array;
+    Expr* start;  // Can be NULL for [:end]
+    Expr* end;    // Can be NULL for [start:]
+} SliceExpr;
 
 struct Expr {
     enum {
@@ -55,7 +74,10 @@ struct Expr {
         EXPR_VARIABLE,
         EXPR_CALL,
         EXPR_ARRAY,
-        EXPR_INDEX
+        EXPR_INDEX,
+        EXPR_DICT,
+        EXPR_TUPLE,
+        EXPR_SLICE
     } type;
     union {
         NumberExpr number;
@@ -66,6 +88,9 @@ struct Expr {
         CallExpr call;
         ArrayExpr array;
         IndexExpr index;
+        DictExpr dict;
+        TupleExpr tuple;
+        SliceExpr slice;
     } as;
 };
 
@@ -113,7 +138,6 @@ typedef struct {
     Stmt* body;      // Loop body
 } ForStmt;
 
-
 struct Stmt {
     enum {
         STMT_PRINT,
@@ -124,7 +148,9 @@ struct Stmt {
         STMT_WHILE,
         STMT_PROC,
         STMT_FOR,
-        STMT_RETURN
+        STMT_RETURN,
+        STMT_BREAK,
+        STMT_CONTINUE
     } type;
     union {
         PrintStmt print;
@@ -140,9 +166,7 @@ struct Stmt {
     Stmt* next;
 };
 
-
-
-// Constructors
+// Expression Constructors
 Expr* new_number_expr(double value);
 Expr* new_binary_expr(Expr* left, Token op, Expr* right);
 Expr* new_variable_expr(Token name);
@@ -152,7 +176,11 @@ Expr* new_bool_expr(int value);
 Expr* new_nil_expr();
 Expr* new_array_expr(Expr** elements, int count);
 Expr* new_index_expr(Expr* array, Expr* index);
+Expr* new_dict_expr(char** keys, Expr** values, int count);
+Expr* new_tuple_expr(Expr** elements, int count);
+Expr* new_slice_expr(Expr* array, Expr* start, Expr* end);
 
+// Statement Constructors
 Stmt* new_print_stmt(Expr* expression);
 Stmt* new_expr_stmt(Expr* expression);
 Stmt* new_let_stmt(Token name, Expr* initializer);
@@ -162,5 +190,7 @@ Stmt* new_while_stmt(Expr* condition, Stmt* body);
 Stmt* new_proc_stmt(Token name, Token* params, int param_count, Stmt* body);
 Stmt* new_for_stmt(Token variable, Expr* iterable, Stmt* body);
 Stmt* new_return_stmt(Expr* value);
+Stmt* new_break_stmt();
+Stmt* new_continue_stmt();
 
 #endif
