285622dd1cca955f73fd07432d7be2509554ddb5
Night-Traders-Dev <quegmeister@gmail.com>
2026-03-09 13:48:15 -0400

    Update

---
+++ build_sage/CMakeFiles/sage.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kraken/Devel/sagelang/src/ast.c" "CMakeFiles/sage.dir/src/ast.c.o" "gcc" "CMakeFiles/sage.dir/src/ast.c.o.d"
+  "/home/kraken/Devel/sagelang/src/codegen.c" "CMakeFiles/sage.dir/src/codegen.c.o" "gcc" "CMakeFiles/sage.dir/src/codegen.c.o.d"
+  "/home/kraken/Devel/sagelang/src/compiler.c" "CMakeFiles/sage.dir/src/compiler.c.o" "gcc" "CMakeFiles/sage.dir/src/compiler.c.o.d"
+  "/home/kraken/Devel/sagelang/src/constfold.c" "CMakeFiles/sage.dir/src/constfold.c.o" "gcc" "CMakeFiles/sage.dir/src/constfold.c.o.d"
+  "/home/kraken/Devel/sagelang/src/dce.c" "CMakeFiles/sage.dir/src/dce.c.o" "gcc" "CMakeFiles/sage.dir/src/dce.c.o.d"
+  "/home/kraken/Devel/sagelang/src/env.c" "CMakeFiles/sage.dir/src/env.c.o" "gcc" "CMakeFiles/sage.dir/src/env.c.o.d"
+  "/home/kraken/Devel/sagelang/src/formatter.c" "CMakeFiles/sage.dir/src/formatter.c.o" "gcc" "CMakeFiles/sage.dir/src/formatter.c.o.d"
+  "/home/kraken/Devel/sagelang/src/gc.c" "CMakeFiles/sage.dir/src/gc.c.o" "gcc" "CMakeFiles/sage.dir/src/gc.c.o.d"
+  "/home/kraken/Devel/sagelang/src/heartbeat.c" "CMakeFiles/sage.dir/src/heartbeat.c.o" "gcc" "CMakeFiles/sage.dir/src/heartbeat.c.o.d"
+  "/home/kraken/Devel/sagelang/src/inline.c" "CMakeFiles/sage.dir/src/inline.c.o" "gcc" "CMakeFiles/sage.dir/src/inline.c.o.d"
+  "/home/kraken/Devel/sagelang/src/interpreter.c" "CMakeFiles/sage.dir/src/interpreter.c.o" "gcc" "CMakeFiles/sage.dir/src/interpreter.c.o.d"
+  "/home/kraken/Devel/sagelang/src/lexer.c" "CMakeFiles/sage.dir/src/lexer.c.o" "gcc" "CMakeFiles/sage.dir/src/lexer.c.o.d"
+  "/home/kraken/Devel/sagelang/src/linter.c" "CMakeFiles/sage.dir/src/linter.c.o" "gcc" "CMakeFiles/sage.dir/src/linter.c.o.d"
+  "/home/kraken/Devel/sagelang/src/llvm_backend.c" "CMakeFiles/sage.dir/src/llvm_backend.c.o" "gcc" "CMakeFiles/sage.dir/src/llvm_backend.c.o.d"
+  "/home/kraken/Devel/sagelang/src/lsp.c" "CMakeFiles/sage.dir/src/lsp.c.o" "gcc" "CMakeFiles/sage.dir/src/lsp.c.o.d"
+  "/home/kraken/Devel/sagelang/src/main.c" "CMakeFiles/sage.dir/src/main.c.o" "gcc" "CMakeFiles/sage.dir/src/main.c.o.d"
+  "/home/kraken/Devel/sagelang/src/module.c" "CMakeFiles/sage.dir/src/module.c.o" "gcc" "CMakeFiles/sage.dir/src/module.c.o.d"
+  "/home/kraken/Devel/sagelang/src/parser.c" "CMakeFiles/sage.dir/src/parser.c.o" "gcc" "CMakeFiles/sage.dir/src/parser.c.o.d"
+  "/home/kraken/Devel/sagelang/src/pass.c" "CMakeFiles/sage.dir/src/pass.c.o" "gcc" "CMakeFiles/sage.dir/src/pass.c.o.d"
+  "/home/kraken/Devel/sagelang/src/stdlib.c" "CMakeFiles/sage.dir/src/stdlib.c.o" "gcc" "CMakeFiles/sage.dir/src/stdlib.c.o.d"
+  "/home/kraken/Devel/sagelang/src/typecheck.c" "CMakeFiles/sage.dir/src/typecheck.c.o" "gcc" "CMakeFiles/sage.dir/src/typecheck.c.o.d"
+  "/home/kraken/Devel/sagelang/src/value.c" "CMakeFiles/sage.dir/src/value.c.o" "gcc" "CMakeFiles/sage.dir/src/value.c.o.d"
+  "" "sage" "gcc" "CMakeFiles/sage.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
