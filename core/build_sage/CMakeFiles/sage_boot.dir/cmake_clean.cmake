0a95242b7893ca4778bd3d7383c8a1eee24137dd
Night-Traders-Dev <quegmeister@gmail.com>
2026-07-10 08:30:41 -0400

    [UPDATE]

---
+++ core/build_sage/CMakeFiles/sage_boot.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sage_boot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sage_boot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ core/build_sage/CMakeFiles/test_selfhost_lexer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for test_selfhost_lexer.
+# This may be replaced when dependencies are built.
