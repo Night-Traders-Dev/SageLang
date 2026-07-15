b8d1c021699219f15b694c3703733e577068ed0c
Night-Traders-Dev <quegmeister@gmail.com>
2026-07-15 08:41:54 -0400

    Remove build_sage artifacts from git tracking

--- core/build_sage/CMakeFiles/test_selfhost_bootstrap.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_selfhost_bootstrap"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_selfhost_bootstrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- core/build_sage/CMakeFiles/test_selfhost_parser.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_selfhost_parser"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_selfhost_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
